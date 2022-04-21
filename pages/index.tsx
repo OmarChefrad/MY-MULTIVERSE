618236699a9ab2765a98c54a5bacb9c710d7bd13
Omar <omarkiojoo@gmail.com>
2022-04-21 19:55:37 +0000

    Fair deal

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home = ({ collections }: Props) => {
           href="https://res.cloudinary.com/adonis1/image/upload/v1649611094/STOOLED-modified_trohmc.png"
         />
       </Head>
-      <header className="items-centre flex justify-between pb-10 lg:pb-20 backdrop-blur-3xl">
+      <header className="items-centre flex justify-between pt-10 pb-5 backdrop-blur-3xl lg:pt-5 lg:pb-20">
         <h1 className="w-full cursor-pointer text-3xl font-extralight sm:w-full lg:text-4xl">
           <span className="font-extrabold  underline decoration-rose-400">
             NFT'S
